b9e57ba8009cf3803c215ebc23a0370ad214ef35
PuzzledPlane <derek_campaniello@student.uml.edu>
2017-10-05 13:37:29 -0400

    Update

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { ImageFilterPipe } from './image/shared/filter.pipe';
+import { ImageService } from './image/shared/image.service';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ImageDetailComponent } from './image/image-detail.component';
+import { appRoutes } from '../routes'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    GalleryComponent,
+    ImageFilterPipe,
+    ImageDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [ImageService, ImageFilterPipe],
+  bootstrap: [AppComponent]
+})
+export class AppModule { 
+    
+}
